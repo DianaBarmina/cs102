b0aedce7b6a4778ad0500a4fbb633546333b55ab
Diana <diana.barmina.2003@mail.ru>
2022-03-22 15:53:56 +0300

    5labaapi

--- homework05/research/network.py
+++ homework05/research/network.py
@@ -5,7 +5,8 @@ import community as community_louvain
 import matplotlib.pyplot as plt
 import networkx as nx
 import pandas as pd
-from homework05.vkapi.friends import get_friends, get_mutual
+
+#from homework05.vkapi.friends import get_friends, get_mutual
 
 
 def ego_network(
